e176db61205f53f509405ecb396b4b40a489b057
Arun-KumarH <arun.kumar@n-fuse.co>
2024-03-14 16:08:25 +0100

    fix: up roleScoping entity key name

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -229,7 +229,7 @@ export async function checkAccessRequest(ctx: ACSClientContext, resource: Resour
 
   let result: DecisionResponse | PolicySetRQResponse;
   try {
-    result = await accessRequest(subject, resource, action, ctx, { operation, roleScopingEntityURN: cfg?.get('authorization:urns:organization') });
+    result = await accessRequest(subject, resource, action, ctx, { operation, roleScopingEntityURN: cfg?.get('authorization:urns:roleScopingEntityURN') });
   } catch (err) {
     return {
       decision: Response_Decision.DENY,
